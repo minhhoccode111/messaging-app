f40f7654367e59b91bdd16dd9866b0fee46df058
minhhoccode111 <minhhoccode111@gmail.com>
2024-03-22 11:09:01 +0700

    Add: GET /chat/groups controllers

--- src/controllers/chatGroupController.js
+++ src/controllers/chatGroupController.js
@@ -18,14 +18,43 @@ const mongoose = require('mongoose');
 
 // get all group that visible to current logged in user (joined, public)
 module.exports.chat_all_group_get = asyncHandler(async (req, res, next) => {
-  res.sendStatus(200);
-  // res.send('chat all group get: not implemented');
+  // first get all current logged in user joined group references
+  let joinedGroups = await GroupMember.find({ user: req.user }).sort({ isCreator: 1 }).populate('group', 'name public avatarLink createdAt').exec();
+
+  joinedGroups = joinedGroups.map((ref) => ({
+    // extract needed fields from populated group field
+    _id: ref?.group?._id,
+    name: ref?.group?.name,
+    public: ref?.group?.public,
+    avatarLink: ref?.group?.avatarLink,
+    createdAt: ref?.group?.createdAt,
+    isCreator: ref.isCreator,
+    // joinedAt:
+  }));
+
+  // console.log(joinedGroups);
+
+  // find in g
+  const notJoinedGroups = await Group.find({ _id: { $nin: joinedGroups } }, 'name public avatarLink createdAt').exec();
+
+  // console.log(`notJoinedGroups belike: `, notJoinedGroups);
+
+  const publicGroups = notJoinedGroups.filter((gr) => gr.public);
+  const privateGroups = notJoinedGroups.filter((gr) => !gr.public);
+
+  // console.log(`publicGroups belike: `, publicGroups);
+  // console.log(`privateGroups belike: `, privateGroups);
+
+  res.json({ requestedUser: req.user, joinedGroups, publicGroups, privateGroups });
 });
 
 // current logged in user create a new group (and be group's creator)
-module.exports.chat_all_group_post = asyncHandler(async (req, res, next) => {
-  res.send('chat all group post: not implemented');
-});
+module.exports.chat_all_group_post = [
+  body(`name`),
+  asyncHandler(async (req, res, next) => {
+    res.send('chat all group post: not implemented');
+  }),
+];
 
 // get conversation with a specific group
 module.exports.chat_group_get = asyncHandler(async (req, res, next) => {
@@ -47,7 +76,6 @@ module.exports.chat_group_put = asyncHandler(async (req, res, next) => {
   res.send('chat group put: not implemented');
 });
 
-
 // get all group's members
 module.exports.chat_group_all_members_get = asyncHandler(async (req, res, next) => {
   res.send('chat group all members get: not implemented');
@@ -62,4 +90,3 @@ module.exports.chat_group_all_members_post = asyncHandler(async (req, res, next)
 module.exports.chat_group_member_delete = asyncHandler(async (req, res, next) => {
   res.send('chat group all members post: not implemented');
 });
-
